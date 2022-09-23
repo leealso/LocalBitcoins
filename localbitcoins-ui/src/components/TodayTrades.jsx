f9f117f06c57faf49eae9962f515c51db9aceeb4
Lee Alfaro Soto <leealfarosoto@Lees-MacBook-Pro-2.local>
2022-09-22 21:26:30 -0600

    Added refresh button feature

--- localbitcoins-ui/src/components/TodayTrades.jsx
+++ localbitcoins-ui/src/components/TodayTrades.jsx
@@ -1,6 +1,5 @@
 import PropTypes from 'prop-types'
 import TradesGrid from './TradesGrid'
-import MobileTradesGrid from './MobileTradesGrid'
 import DatePickerButton from './DatePickerButton'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
@@ -8,17 +7,17 @@ import Col from 'react-bootstrap/Col'
 import { connect } from 'react-redux'
 import { setSelectedDate } from '../store/reducers/tradeSlice.ts'
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { useGetTradesQuery } from '../services/localBitcoinsService'
 import LoadingSpinner from './LoadingSpinner'
 import LoadingButton from './LoadingButton'
 
 const TodayTrades = ({ date }) => {
     const dispatch = useDispatch()
-    const selectedDate = useSelector(state => state.trades.selectedDate)
-    var startDate = new Date(selectedDate)
+    let startDate = new Date(date)
     startDate.setHours(0, 0, 0, 0)
-    var endDate = new Date(startDate)
+    let isToday = startDate.toDateString() === new Date().toDateString()
+    let endDate = new Date(startDate)
     endDate.setTime(endDate.getTime() + (24*60*60*1000))
     const where = {
         and: [
@@ -35,8 +34,7 @@ const TodayTrades = ({ date }) => {
         ]
         
     }
-    const { data: trades, isLoading } = useGetTradesQuery({ where: where })
-    // 
+    const { data: trades, isLoading, refetch } = useGetTradesQuery({ where: where })
     return (
         <Container>
             <Row>
@@ -46,7 +44,7 @@ const TodayTrades = ({ date }) => {
                 <Col xs="4" sm="2">
                     <div className="d-flex h-100 align-items-center">
                         <DatePickerButton date={date} onDateChange={(date) => dispatch(setSelectedDate(date.getTime()))} />
-                        <div className="ml-2"><LoadingButton isLoading={isLoading} handleClick={() => {}} /></div>
+                        <LoadingButton isLoading={isLoading} handleClick={() => isToday ? refetch() : null} />
                     </div>
                 </Col>
             </Row>
