f17f9f4d7c422d322709475d1b11d41792ffcbd8
Lee Alfaro Soto <leealfarosoto@Lees-MacBook-Pro-2.local>
2022-09-27 21:16:05 -0600

    Added percentchang fields

--- LocalBitcoinsAPI/Models/CoinMarketCap/QuoteUsd.cs
+++ LocalBitcoinsAPI/Models/CoinMarketCap/QuoteUsd.cs
@@ -6,6 +6,18 @@ public class QuoteUsd
 {
     public decimal Price { get; set; }
 
+    [JsonProperty("percent_change_24h")]
+    public decimal PercentChange24h { get; set; }
+
+    [JsonProperty("percent_change_7d")]
+    public decimal PercentChange7d { get; set; }
+
+    [JsonProperty("percent_change_30d")]
+    public decimal PercentChange30d { get; set; }
+
+    [JsonProperty("percent_change_90d")]
+    public decimal PercentChange90d { get; set; }
+
     [JsonProperty("last_updated")]
     public DateTime LastUpdated { get; set; }
     
--- LocalBitcoinsAPI/Models/Quote.cs
+++ LocalBitcoinsAPI/Models/Quote.cs
@@ -8,11 +8,23 @@ public class Quote
 
     public decimal Price { get;set; }
 
+    public decimal PercentChange24h { get; set; }
+
+    public decimal PercentChange7d { get; set; }
+
+    public decimal PercentChange30d { get; set; }
+
+    public decimal PercentChange90d { get; set; }
+
     public DateTime LastUpdated { get;set; }
     
     public Quote(CryptoCurrency cryptoCurrency)
     {
         Symbol = cryptoCurrency.Symbol;
+        PercentChange24h = cryptoCurrency.Quote.Usd.PercentChange24h;
+        PercentChange7d = cryptoCurrency.Quote.Usd.PercentChange7d;
+        PercentChange30d = cryptoCurrency.Quote.Usd.PercentChange30d;
+        PercentChange90d = cryptoCurrency.Quote.Usd.PercentChange90d;
         Price = cryptoCurrency.Quote.Usd.Price;
         LastUpdated = cryptoCurrency.Quote.Usd.LastUpdated;
     }
