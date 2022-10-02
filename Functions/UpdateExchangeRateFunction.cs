84e2f4e296ebd4a43118afc4e37476a405d026b8
Lee Alfaro Soto <leealfarosoto@Lees-MacBook-Pro-2.local>
2022-10-01 20:16:57 -0600

    Logging

--- Functions/UpdateExchangeRateFunction.cs
+++ Functions/UpdateExchangeRateFunction.cs
@@ -11,17 +11,27 @@ namespace LocalBitcoins.Functions
     {
         private readonly IExchangeRateService _exchangeRateService;
 
-        public UpdateExchangeRateFunction(IExchangeRateService exchangeRateService)
+        private readonly ILogger<UpdateExchangeRateFunction> _logger;
+
+        public UpdateExchangeRateFunction(IExchangeRateService exchangeRateService, ILogger<UpdateExchangeRateFunction> logger)
         {
             _exchangeRateService = exchangeRateService;
+            _logger = logger;
         }
         
         [FunctionName("UpdateExchangeRateFunction")]
         public async Task Run([TimerTrigger("0 0 0,12,18,19 * * *")]TimerInfo myTimer, ILogger log, CancellationToken cancellationToken = default)
         {
-            var startDate = new DateTime(2022, 01, 01);
-            var endDate = new DateTime(2022, 12, 12);
-            await _exchangeRateService.UpdateExchangeRatesAsync(startDate, endDate, cancellationToken);
+            try
+            {
+                var startDate = new DateTime(2022, 01, 01);
+                var endDate = new DateTime(2022, 12, 12);
+                await _exchangeRateService.UpdateExchangeRatesAsync(startDate, endDate, cancellationToken);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+            }
         }
     }
 }
