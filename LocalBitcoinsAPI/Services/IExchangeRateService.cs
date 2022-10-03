93dcb0c5715d9273989f7677fdc98490b0b71339
Lee Alfaro Soto <leealfarosoto@Lees-MacBook-Pro-2.local>
2022-10-02 20:55:59 -0600

    Updated to use propietary exchange rate for ad prices

--- LocalBitcoinsAPI/Services/IExchangeRateService.cs
+++ LocalBitcoinsAPI/Services/IExchangeRateService.cs
@@ -8,4 +8,7 @@ public interface IExchangeRateService
     Task<ExchangeRate> AddAsync(string fromCurrencyCode, string toCurrencyCode, DateTime date, decimal value, CancellationToken cancellationToken = default);
 
     Task<ExchangeRate> GetExchangeRateAsync(DateTime date, string fromCurrencyCode, string toCurrencyCode, CancellationToken cancellationToken = default);
+
+    Task<ExchangeRate> GetExchangeRateAsync(DateTime date, CancellationToken cancellationToken = default) 
+        => GetExchangeRateAsync(date, CurrencyCode.USD, CurrencyCode.CRC, cancellationToken);
 }
