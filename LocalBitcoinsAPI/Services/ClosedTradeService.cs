26845d41f483ef780388d7a3f0a63e92fb8254f4
Lee Alfaro Soto <leealfarosoto@Lees-MacBook-Pro-2.local>
2022-10-08 16:00:19 -0600

    Added mutation to insert closed trade

--- LocalBitcoinsAPI/Services/ClosedTradeService.cs
+++ LocalBitcoinsAPI/Services/ClosedTradeService.cs
@@ -13,6 +13,24 @@ public class ClosedTradeService : IClosedTradeService, IAsyncDisposable
         _dbContext = dbContextFactory.CreateDbContext();
     }
 
+    public async Task<ClosedTrade> AddAsync(ClosedTrade closedTrade, CancellationToken cancellationToken = default)
+    {
+        var result = await _dbContext.AddAsync(closedTrade, cancellationToken);
+        
+        var trade = await _dbContext.Trades.SingleOrDefaultAsync(x => 
+            x.Date == closedTrade.ClosedAt
+            && x.AmountBtc == closedTrade.AmountBtc
+            && x.AmountFiat == closedTrade.AmountFiat
+            && x.CurrencyCode == closedTrade.CurrencyCode,
+            cancellationToken
+        );
+        if (trade != null)
+            trade.ContactId = closedTrade.ContactId;
+            
+        await _dbContext.SaveChangesAsync(cancellationToken);
+        return result.Entity;
+    }
+
     public IQueryable<ClosedTrade> GetClosedTrades()
     {
         return _dbContext.ClosedTrades.AsQueryable();
--- LocalBitcoinsAPI/Services/IClosedTradeService.cs
+++ LocalBitcoinsAPI/Services/IClosedTradeService.cs
@@ -4,5 +4,7 @@ namespace LocalBitcoinsAPI.Services;
 
 public interface IClosedTradeService
 {
+    Task<ClosedTrade> AddAsync(ClosedTrade closedTrade, CancellationToken cancellationToken = default);
+    
     IQueryable<ClosedTrade> GetClosedTrades();
 }
