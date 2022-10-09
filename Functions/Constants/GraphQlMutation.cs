321314d2e34ea0fd46c3bbdc09218812464518ab
Lee Alfaro Soto <leealfarosoto@Lees-MacBook-Pro-2.local>
2022-10-08 18:06:02 -0600

    Minor fixes for ClosedTrades function

--- Functions/Constants/GraphQlMutation.cs
+++ Functions/Constants/GraphQlMutation.cs
@@ -2,7 +2,7 @@ namespace LocalBitcoins.Functions.Constants;
 
 public static class GraphQlMutation
 {
-    public const string AddClosedTrades = @"mutation mutation($closedTrades:[ClosedTradeInput!]!){addClosedTrades(closedTrades:$closedTrades){contactId}}";
+    public const string AddClosedTrades = @"mutation mutation($closedTrades:[ClosedTradeInput!]!){ response:addClosedTrades(closedTrades:$closedTrades) { contactId } }";
 
-    public const string AddExchangeRate = @"mutation mutation($closedTrade:ClosedTradeInput!){addClosedTrade(closedTrade:$closedTrade){contactId}}";
+    public const string AddExchangeRate = @"mutation mutation($fromCurrencyCode:String! $toCurrencyCode:String! $date:DateTime! $value:Decimal!){ response:addExchangeRate(fromCurrencyCode: $fromCurrencyCode toCurrencyCode: $toCurrencyCode date: $date value: $value) { id } }";
 }
--- Functions/Constants/GraphQlQuery.cs
+++ Functions/Constants/GraphQlQuery.cs
@@ -2,5 +2,5 @@ namespace LocalBitcoins.Functions.Constants;
 
 public static class GraphQlQuery
 {
-    public const string MissingContactIds = @"query query($closedAt:DateTime!$contactIds:[Int!]!){missingContactIds(closedAt:$closedAtcontactIds:$contactIds)}";
+    public const string MissingContactIds = @"query query($closedAt:DateTime! $contactIds:[Int!]!){ response:missingContactIds(closedAt:$closedAt contactIds:$contactIds) }";
 }
