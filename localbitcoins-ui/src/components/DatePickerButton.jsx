bc2ec7f084e8b399cb8943ae7226f655631d65a2
Lee Alfaro Soto <leealfarosoto@Lees-MacBook-Pro-2.local>
2022-10-09 21:38:02 -0600

    Updated monthly page to show charts

--- localbitcoins-ui/src/components/DatePickerButton.jsx
+++ localbitcoins-ui/src/components/DatePickerButton.jsx
@@ -3,7 +3,7 @@ import DatePicker from 'react-datepicker';
 import Button from 'react-bootstrap/Button';
 import { forwardRef } from 'react';
 
-const DatePickerButton = ({ date, onDateChange }) => {
+const DatePickerButton = ({ date, onDateChange, dateFormat, showMonthYearPicker, showFullMonthYearPicker }) => {
     const CustomInput = forwardRef(({ value, onClick }, ref) => (
         <Button className="float-end" onClick={onClick} ref={ref}>
             {value}
@@ -15,16 +15,25 @@ const DatePickerButton = ({ date, onDateChange }) => {
             onChange={(date) => onDateChange(date)}
             customInput={<CustomInput />}
             wrapperClassName="d-flex"
+            dateFormat={dateFormat}
+            showMonthYearPicker={showMonthYearPicker}
+            showFullMonthYearPicker={showFullMonthYearPicker}
         />
     );
 }
 
 DatePickerButton.defaultProps = {
-    date: new Date().getTime()
+    date: new Date().getTime(),
+    dateFormat: 'MM/dd/yyyy',
+    showMonthYearPicker: false,
+    showFullMonthYearPicker: false
 }
 
 DatePickerButton.propTypes = {
-    date: PropTypes.number.isRequired
+    date: PropTypes.number.isRequired,
+    dateFormat: PropTypes.string,
+    showMonthYearPicker: PropTypes.bool,
+    showFullMonthYearPicker: PropTypes.bool
 }
 
 export default DatePickerButton;
--- localbitcoins-ui/src/components/MonthlyTrades.jsx
+++ localbitcoins-ui/src/components/MonthlyTrades.jsx
@@ -1,17 +1,17 @@
 import PropTypes from 'prop-types'
-import TradesGrid from './TradesGrid'
+import { Chart } from 'react-google-charts'
 import DatePickerButton from './DatePickerButton'
 import Container from 'react-bootstrap/Container'
 import { connect } from 'react-redux'
 import { setSelectedDate } from '../store/reducers/tradeSlice.ts'
 import React from 'react'
 import { useDispatch } from 'react-redux'
-import { useGetSummaryQuery } from '../services/localBitcoinsApiService'
+import { useGetDailySummariesQuery, useGetSummaryQuery } from '../services/localBitcoinsApiService'
 import ContentHeader from './ContentHeader'
 import ContentBody from './ContentBody'
 import SummaryContainer from './SummaryContainer'
 import SummaryRow from './SummaryRow'
-import { formatNumber } from '../stringUtility'
+import { formatDate, formatNumber } from '../stringUtility'
 
 const MonthlyTrades = ({ date }) => {
     const selectedDate = new Date(date)
@@ -21,21 +21,75 @@ const MonthlyTrades = ({ date }) => {
     const dispatch = useDispatch()
 
     const { data: summary, isLoading: isLoadingSummary, isFetching: isFetchingSummary, refetch: refetchSummary } = useGetSummaryQuery({ startDate: startDate.toISOString(), endDate: endDate.toISOString() })
+    const { data: dailySummary, isLoading: isLoadingDailySummary, isFetching: isFetchingDailySummary, refetch: refetchDailySummary } = useGetDailySummariesQuery({ startDate: startDate.toISOString(), endDate: endDate.toISOString() })
 
     const fiatVolumeFormatted = formatNumber(summary?.response?.fiatVolume, '₡', 2)
     const priceFormatted = formatNumber(summary?.response?.price, '₡', 2)
-    const priceReference = summary?.response?.closedPrice / summary?.response?.price
+    const priceReference = 1 - summary?.response?.closedPrice / summary?.response?.price
     const isCurrentMonth = selectedDate.getMonth() === new Date().getMonth()
+
+    let data = [
+        ['Transactions', 'Total', 'Closed']
+    ]
+    let dataBtcVolume = [
+        ['BTC Volume', 'Total', 'Closed']
+    ]
+    dailySummary?.response.forEach(x => {
+        const formattedDate = formatDate(new Date(x.startDate))
+        data.push([formattedDate, x.transactionCount, x.closedTransactionCount])
+        dataBtcVolume.push([formattedDate, x.btcVolume, x.closedBtcVolume])
+    })
+    const options = {
+        title: 'Transactions',
+        backgroundColor: 'transparent',
+        colors: ['#ee5f5b','#62c462'],
+        legend: 'none',
+        titleTextStyle: {
+            color: "#e9ecef",
+            fontSize: 20
+        },
+        vAxsis: {
+            titleTextStyle: {
+                color: "#e9ecef",
+            }
+        },
+        hAxsis: {
+            titleTextStyle: {
+                color: "#e9ecef",
+            }
+        }
+    }
+    const optionsBtcVolume = {
+        title: 'BTC Volume',
+        backgroundColor: 'transparent',
+        colors: ['#ee5f5b','#62c462'],
+        legend: 'none',
+        titleTextStyle: {
+            color: "#e9ecef",
+            fontSize: 20
+        },
+        vAxsis: {
+            titleTextStyle: {
+                color: "#e9ecef",
+            }
+        },
+        hAxsis: {
+            titleTextStyle: {
+                color: "#e9ecef",
+            }
+        }
+    }
     const refresh = () => {
         if (isCurrentMonth) {
             refetchSummary()
+            refetchDailySummary()
         }
     }
-    const isLoading = isLoadingSummary || isFetchingSummary
+    const isLoading = isLoadingSummary || isFetchingSummary || isLoadingDailySummary || isFetchingDailySummary
     return (
         <Container className='pt-2'>
             <ContentHeader title={'Monthly Trades'} isLoading={isLoading} onRefreshClick={refresh}>
-                <DatePickerButton date={date} onDateChange={(date) => dispatch(setSelectedDate(date.getTime()))} />
+                <DatePickerButton date={date} dateFormat='MM/yyyy' showFullMonthYearPicker={true} showMonthYearPicker={true} onDateChange={(date) => dispatch(setSelectedDate(date.getTime()))} />
             </ContentHeader>
             <ContentBody isLoading={isLoading}>
                 <SummaryContainer>
@@ -44,6 +98,19 @@ const MonthlyTrades = ({ date }) => {
                     <SummaryRow label={'Fiat Volume'} value={`${fiatVolumeFormatted}`} reference={summary?.response?.closedFiatVolumePercentage} />
                     <SummaryRow label={'Average Price'} value={`${priceFormatted}`} reference={priceReference} />
                 </SummaryContainer>
+                <Chart
+                    chartType='AreaChart'
+                    data={data}
+                    options={options}
+                    width={'100%'}
+                />
+                <Chart
+                    chartType='AreaChart'
+                    data={dataBtcVolume}
+                    options={optionsBtcVolume}
+                    width={'100%'}
+                    className='text-light'
+                />
             </ContentBody>
         </Container>
     )
